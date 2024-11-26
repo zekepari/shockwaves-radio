6b16189934e07633311ed2db613a53bdf07e0b3c
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-11-26 20:07:42 +1000

    add back tune of the week

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -31,16 +31,22 @@ export default async function Page() {
             <RecentlyPlayed/>
           </div>
         </section>
-        <section className="container max-w-screen-lg space-y-4">
-          <h2 className="font-bold text-2xl">Support SWR</h2>
-          <div className="flex bg-base-100 rounded-box shadow-xl">
-            <div className="p-2 w-full overflow-hidden">
-              <h2 className="font-bold text-xl truncate">Want to support the station?</h2>
-              <p className="opacity-75">Use the button below to support your favourite station!</p>
-              <Link href={"https://ko-fi.com/shockwavesradio"} target="_blank" className="btn btn-primary btn-block btn-sm">
-                Ko-Fi
-              </Link>
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 container max-w-screen-lg">
+          <div className="space-y-4">
+            <h2 className="font-bold text-2xl">Support SWR</h2>
+            <div className="flex bg-base-100 rounded-box min-w-96 shadow-xl">
+              <div className="p-2 w-full overflow-hidden">
+                <h2 className="font-bold text-xl truncate">Want to support the station?</h2>
+                <p className="opacity-75">Use the button below to support your favourite station!</p>
+                <Link href={"https://ko-fi.com/shockwavesradio"} target="_blank" className="btn btn-primary btn-block btn-sm">
+                  Ko-Fi
+                </Link>
+              </div>
             </div>
+          </div>
+          <div className="space-y-4">
+            <h2 className="font-bold text-2xl">Tune of the week</h2>
+            <SongCard name="wokeuplikethis*" artist="Playboi Carti" timeAgo={1730781783} url=""/>
           </div>
         </section>
       </div>
